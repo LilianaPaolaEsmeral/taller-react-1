8e43ba68ab94577e7f7ced6333498f84ed2cd97e
Liliana Paola Esmeral <lilianaesmeral951@gmail.com>
2022-10-12 01:40:52 -0500

    segundo commit

--- src/components/PeticionApi.jsx
+++ src/components/PeticionApi.jsx
@@ -1,14 +1,43 @@
 import React from 'react'
 
 const PeticionApi = () => {
-    const obtenerPersonajes = async () => {
-        const res = await fetch("https://pokeapi.co/api/v2/pokemon/");
+    const [siguiente, setSiguiente] = React.useState("https://pokeapi.co/api/v2/pokemon/");
+    const [previous, setPrevius] = React.useState("");
+    const [listaPokemones, setListaPokemones] = React.useState([]);
+
+    const obtenerPersonajes = async (url) => {
+        const res = await fetch(url);
         const { results, next, previous } = await res.json();
-        // const listaPokemones = results
+
+        const lista = await Promise.all(
+            results.map(async ({ name, url }) => {
+                const res2 = await obtenerDetalle(url);
+                const { abilities, sprites } = res2;
+                const urlFoto = sprites.front_default;
+                const habilidades = abilities.map(({ ability }) => ability.name);
+                const pokemon = {
+                    nombre: name,
+                    foto: urlFoto,
+                    habilidades
+                }
+                return pokemon
+            }));
+        await setSiguiente(next);
+        await setPrevius(previous);
+        await setListaPokemones(lista);
     }
+    React.useEffect(() => {
+        obtenerPersonajes(siguiente);
+    }, []);
+
+    const obtenerDetalle = async (url) => {
+        const res = await fetch(url);
+        return await res.json();
+    }
+
     return (
         <div>
-
+            
         </div>
     )
 }
