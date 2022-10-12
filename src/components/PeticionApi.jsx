8878ea00ebc929d3be76a0f79dce72383b0cadf5
Liliana Paola Esmeral <lilianaesmeral951@gmail.com>
2022-10-12 01:49:50 -0500

    tercer commit

--- src/components/PeticionApi.jsx
+++ src/components/PeticionApi.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const PeticionApi = () => {
-    const [siguiente, setSiguiente] = React.useState("https://pokeapi.co/api/v2/pokemon/");
+    const [siguiente, setSiguiente] = React.useState("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20");
     const [previous, setPrevius] = React.useState("");
     const [listaPokemones, setListaPokemones] = React.useState([]);
 
@@ -12,13 +12,14 @@ const PeticionApi = () => {
         const lista = await Promise.all(
             results.map(async ({ name, url }) => {
                 const res2 = await obtenerDetalle(url);
-                const { abilities, sprites } = res2;
+                const { abilities, sprites, id } = res2;
                 const urlFoto = sprites.front_default;
                 const habilidades = abilities.map(({ ability }) => ability.name);
                 const pokemon = {
                     nombre: name,
                     foto: urlFoto,
-                    habilidades
+                    habilidades,
+                    id
                 }
                 return pokemon
             }));
@@ -28,16 +29,42 @@ const PeticionApi = () => {
     }
     React.useEffect(() => {
         obtenerPersonajes(siguiente);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     const obtenerDetalle = async (url) => {
         const res = await fetch(url);
         return await res.json();
     }
+    const atras = () => {
+        if (previous.length !== "") {
+            obtenerPersonajes(previous);
+        }
+    }
 
+    const sigiente = () => {
+        obtenerPersonajes(siguiente);
+    }
     return (
         <div>
-            
+            <h1>
+                Peticion a PokeApi
+            </h1>
+            <button onClick={atras}>Atrás</button>
+            <button onClick={sigiente}>Siguiente</button>
+            {
+                listaPokemones.map(({id,nombre,foto,habilidades})=> (
+                    <div key={id}>
+                        <h4>{id}. {nombre}</h4>
+                        <img src={foto} alt={nombre} />
+                        <ul>
+                            {
+                                habilidades.map((item,index) => <li key={index}>{item}</li>)
+                            }
+                        </ul>
+                    </div>
+                ))
+            }
         </div>
     )
 }
